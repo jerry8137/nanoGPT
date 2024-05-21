0734d93a8765bf26aa1ad676c8963226dcf974e9
Jerry Hou <jerry8137@gmail.com>
2024-05-21 21:56:56 +0800

    get vocabulary

--- train.py
+++ train.py
@@ -1,6 +1,10 @@
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
-print("length of text: ", len(text))
+print('length of text: ', len(text))
 
-print(text[:1000])
+# print(text[:1000])
+chars = sorted(list(set(text)))
+vocabulary_size = len(chars)
+print(''.join(chars))
+print('vocabulary size: ', vocabulary_size)
