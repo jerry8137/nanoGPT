cffa8170f846b0a762be23fb0804b69918d038d5
Jerry Hou <jerry8137@gmail.com>
2024-05-22 23:09:45 +0800

    get batch

--- dev.py
+++ dev.py
@@ -26,7 +26,7 @@ torch.manual_seed(1337)
 batch_size = 4
 block_size = 8
 
-data = stoi(text)
+data = encode(text)
 n = int(0.9*len(data))
 train_data = data[:n]
 val_data = data[:n]
@@ -35,6 +35,12 @@ val_data = data[:n]
 def get_batch(split):
     data = train_data if split == 'train' else val_data
     ix = torch.randint(len(data) - block_size, (batch_size,))
-    x = torch.stack([data[i:i+block_size] for i in ix])
-    y = torch.stack([data[i+1:i+block_size] for i in ix])
+    x = torch.tensor([data[i:i+block_size] for i in ix])
+    y = torch.tensor([data[i+1:i+block_size] for i in ix])
     return x, y
+
+
+xb, yb = get_batch('train')
+print('inputs:')
+print(xb.shape)
+print(xb)
