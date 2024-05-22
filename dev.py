733ceb3eca7bddf4ad0e6e07b1f3a9fbecc9243f
Jerry Hou <jerry8137@gmail.com>
2024-05-22 23:19:11 +0800

    show the training data

--- dev.py
+++ dev.py
@@ -36,7 +36,7 @@ def get_batch(split):
     data = train_data if split == 'train' else val_data
     ix = torch.randint(len(data) - block_size, (batch_size,))
     x = torch.tensor([data[i:i+block_size] for i in ix])
-    y = torch.tensor([data[i+1:i+block_size] for i in ix])
+    y = torch.tensor([data[i+1:i+block_size+1] for i in ix])
     return x, y
 
 
@@ -44,3 +44,12 @@ xb, yb = get_batch('train')
 print('inputs:')
 print(xb.shape)
 print(xb)
+print('targets:')
+print(yb.shape)
+print(yb)
+
+for b in range(batch_size):
+    for t in range(block_size):
+        context = xb[b, :t+1]
+        target = yb[b, t]
+        print(f"when input is {context.tolist()} the target: {target}")
