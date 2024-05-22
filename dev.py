707c3da84daf6c43d38b951bd9b0de2a9df8a963
Jerry Hou <chunyu.hou@itri.org.tw>
2024-05-22 18:30:23 +0800

    rename and dataloader

--- dev.py
+++ dev.py
@@ -1,3 +1,5 @@
+import torch
+
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
@@ -19,3 +21,20 @@ def decode(input): return ''.join([itos[i] for i in input])
 
 print(encode('hello world'))
 print(decode(encode('hello world')))
+
+torch.manual_seed(1337)
+batch_size = 4
+block_size = 8
+
+data = stoi(text)
+n = int(0.9*len(data))
+train_data = data[:n]
+val_data = data[:n]
+
+
+def get_batch(split):
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size] for i in ix])
+    return x, y
