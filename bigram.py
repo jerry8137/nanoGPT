89718d5b3a91be9c3701de29ef7ba87aac5aaf92
Jerry Hou <jerry8137@gmail.com>
2024-05-23 21:06:58 +0800

    organize dev.py to bigram.py

---
+++ bigram.py
@@ -0,0 +1,62 @@
+import torch
+from bigramLanguageModel import BigramLanguageModel
+from tqdm import tqdm
+
+batch_size = 32
+block_size = 8
+max_iters = 3000
+eval_interval = 300
+learning_rate = 1e-2
+device = "mps" if torch.backends.mps.is_available(
+) else "cuda" if torch.cuda.is_available() else "cpu"
+eval_iters = 200
+
+torch.manual_seed(1337)
+
+with open('input.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+print('length of text: ', len(text))
+
+# print(text[:1000])
+chars = sorted(list(set(text)))
+stoi = {char: i for i, char in enumerate(chars)}
+itos = {i: char for i, char in enumerate(chars)}
+
+
+def encode(input): return [stoi[i] for i in input]
+def decode(input): return ''.join([itos[i] for i in input])
+
+
+data = encode(text)
+n = int(0.9*len(data))
+train_data = data[:n]
+val_data = data[:n]
+
+
+def get_batch(split):
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.tensor([data[i:i+block_size] for i in ix])
+    y = torch.tensor([data[i+1:i+block_size+1] for i in ix])
+    return x, y
+
+
+xb, yb = get_batch('train')
+m = BigramLanguageModel(len(chars))
+
+optimizer = torch.optim.AdamW(m.parameters(), lr=learning_rate)
+
+for steps in tqdm(range(max_iters)):
+    xb, yb = get_batch('train')
+
+    logits, loss = m(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+
+print(loss.item())
+
+idx = torch.zeros((1, 1), dtype=torch.long)
+print(decode(m.generate(idx,
+      max_new_token=500)[0].tolist()))
