f9abc9a6b71fe181475ce3262be37e14945ca07d
Jerry Hou <jerry8137@gmail.com>
2024-05-23 08:59:44 +0800

    add generate

--- bigramLanguageModel.py
+++ bigramLanguageModel.py
@@ -14,9 +14,23 @@ class BigramLanguageModel(nn.Module):
         # idx and targtets are both (B, T)
         logits = self.token_embedding_table(idx)  # (B, T, C)
 
-        B, T, C = logits.shape
-        logits = logits.view(B*T, C)
-        targets = targets.view(B*T)
-        loss = F.cross_entropy(logits, targets)
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
 
         return logits, loss
+
+    def generate(self, idx, max_new_token):
+        for _ in range(max_new_token):
+            logits, loss = self(idx, None)
+            logits = logits[:, -1, :]  # take last one, the pred
+            probs = F.softmax(logits, dim=-1)
+            # choose one from prob (B, 1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat((idx, idx_next), dim=1)
+
+        return idx
